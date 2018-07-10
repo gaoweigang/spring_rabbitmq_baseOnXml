dc03d0a4e0e29fe1113d9a49b708a0f5c16fa171
gaoweigang <13817191469@163.com>
2018-07-10 16:52:53 +0800

    test

---
+++ src/main/java/com/gwg/demo/sender/HelloSender.java
@@ -0,0 +1,39 @@
+package com.gwg.demo.sender;
+
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+
+public class HelloSender {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HelloSender.class);
+	
+	//使用Autowired注解 免去setter方法注入，如果使用setter,还需要在spring配置文件中配置注入对象
+	@Autowired
+    private AmqpTemplate rabbitTemplate;
+	
+	@Value("${rabbitmq.queue}")
+	private String queue;
+ 
+    public void send() {
+    	logger.info("rabbitTemplate :" + rabbitTemplate);
+        String context = "hello gwg " + new Date();
+        logger.info("Sender : " + context);
+        this.rabbitTemplate.convertAndSend(queue, context);
+    }
+
+/*	public AmqpTemplate getRabbitTemplate() {
+		return rabbitTemplate;
+	}
+
+	public void setRabbitTemplate(AmqpTemplate rabbitTemplate) {
+		this.rabbitTemplate = rabbitTemplate;
+	}
+    */
+ 
+}
